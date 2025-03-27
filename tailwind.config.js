8932802509882da31408385359cf35924d0dda47
Nicolas Orgias <norgias@gmail.com>
2025-03-27 02:22:46 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#00B4D8',
+        'secondary': '#7209B7',
+        'accent': '#4CC9F0',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    },
+  },
+  plugins: [],
+};
