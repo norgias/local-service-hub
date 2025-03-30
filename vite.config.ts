b3efb85e9cf624d4906a8c7e36698c7f32f59b72
Nicolas Orgias <norgias@gmail.com>
2025-03-30 17:42:30 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,15 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ['local-service-hub']
+    }
+  },
+  resolve: {
+    alias: {
+      // Define path aliases to help with imports
+      '@': resolve(__dirname, './src'),
+      '@components': resolve(__dirname, './src/components'),
+      '@lib': resolve(__dirname, './src/lib'),
+      '@contexts': resolve(__dirname, './src/contexts')
     }
   }
 });
